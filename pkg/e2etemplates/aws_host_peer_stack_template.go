20a8f4f45e7ebded75671e5dadaa1419483b37d2
Tim Schindler <xh3b4sd@gmail.com>
2018-11-08 16:49:42 +0100

    improves host peer stack template to not use env vars

--- pkg/e2etemplates/aws_host_peer_stack_template.go
+++ pkg/e2etemplates/aws_host_peer_stack_template.go
@@ -1,8 +1,8 @@
 package e2etemplates
 
-// AWSHostVPCStack is deprecated. See awsHostPeerStackTemplate instead.
-const AWSHostVPCStack = `AWSTemplateFormatVersion: 2010-09-09
-Description: CI Host Stack with Peering VPC and route tables
+const awsHostPeerStackTemplate = `
+AWSTemplateFormatVersion: 2010-09-09
+Description: Host Peer Stack with VPC peering and route tables for testing purposes
 Resources:
   VPC:
     Type: AWS::EC2::VPC
@@ -10,24 +10,23 @@ Resources:
       CidrBlock: 10.11.0.0/16
       Tags:
       - Key: Name
-        Value: ${CLUSTER_NAME}
+        Value: {{ .Stack.Name }}
   PeerRouteTable0:
     Type: AWS::EC2::RouteTable
     Properties:
       VpcId: !Ref VPC
       Tags:
       - Key: Name
-        Value: ${AWS_ROUTE_TABLE_0}
+        Value: {{ .RouteTable0.Name }}
   PeerRouteTable1:
     Type: AWS::EC2::RouteTable
     Properties:
       VpcId: !Ref VPC
       Tags:
       - Key: Name
-        Value: ${AWS_ROUTE_TABLE_1}
+        Value: {{ .RouteTable1.Name }}
 Outputs:
   VPCID:
     Description: Accepter VPC ID
     Value: !Ref VPC
-
 `
